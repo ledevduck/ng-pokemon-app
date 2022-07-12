aa37de8116bacb1bd91846bb449ba962a712966a
“ledevduck” <devduck@tutanota.com>
2022-07-12 20:29:15 +0200

    update to use files added in precedent commit (ex at 1h20)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from 'src/mock-pokemon';
+import { Pokemon } from 'src/pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
+  template: `<h1>Liste de pokemons</h1>`
 })
 export class AppComponent implements OnInit {
-  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonList = POKEMONS;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon('Bulbizarre');
+    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemonName: string) {
-    console.log(`Vous avez cliqué sur le ${pokemonName}`);
+  selectPokemon(pokemon: Pokemon) {
+    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
   }
 }
