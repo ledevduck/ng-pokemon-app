971e39aeb31940187e8c5e90432029ec83e41a72
“ledevduck” <devduck@tutanota.com>
2022-07-12 20:01:22 +0200

    add onInit implementation with a console.log using keyword this as an example, update variables name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{ pokemons[0] }}!</h1>`
+  template: `<h1>Welcome to {{ pokemonList[0] }}!</h1>`
 })
-export class AppComponent {
-  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+export class AppComponent implements OnInit {
+  pokemonList = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
 }
