b3b13d5dd005f3926aa14dc58675a1999f9b3d6a
“ledevduck” <devduck@tutanota.com>
2022-07-15 12:26:27 +0200

    OOPSIE, ActivatedRoute is necessary, not Router

--- src/app/detail-pokemon/detail-pokemon.component.ts
+++ src/app/detail-pokemon/detail-pokemon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail-pokemon',
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class DetailPokemonComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
