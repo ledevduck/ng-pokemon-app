c99516733a1326f4fabccd984daac91149364717
“ledevduck” <devduck@tutanota.com>
2022-07-15 09:34:50 +0200

    pokemon selection improve with undefined type handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,20 @@ import { Pokemon } from 'src/pokemon';
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
   }
 
-  selectPokemon(pokemon: MouseEvent) {
-    if (event) {
-      const index: number = +(event.target as HTMLInputElement).value;
-      console.log(`Vous avez cliqué sur le pokemon ${this.pokemonList[index].name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if (pokemon) {
+      console.log(`Vous avez demandé le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokemon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
     }
   }
 }
