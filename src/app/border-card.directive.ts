a05469bd89300bfc6f6a208d673eb92da143a157
“ledevduck” <devduck@tutanota.com>
2022-07-15 10:30:34 +0200

    set methods height and border to borderCardDirective

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appBorderCard]'
 })
 export class BorderCardDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+    this.setHeight(180);
+    this.setBorder('#f5f5f5');
+  }
+
+  setHeight(height: number) {
+    this.el.nativeElement.style.height = `${height}px`;
+  }
+
+  setBorder(color: string) {
+    this.el.nativeElement.style.border = `solid 4px ${color}`
+  }
 
 }
