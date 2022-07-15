7d40801072f708e88f7dadc05977ecd6eaaeba34
“ledevduck” <devduck@tutanota.com>
2022-07-15 12:02:25 +0200

    don't need / for routes, but need to declare more specific routes first to generic routes last, otherwise generic routes will intercept first, order in declaring routes is very important!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,9 @@ import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.componen
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
 const routes: Routes = [
-  { path: '/pokemons', component: ListPokemonComponent },
-  { path: '/pokemon/:id', component: DetailPokemonComponent },
-  { path: '', redirectTo: '/pokemons', pathMatch: 'full' }
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemon/:id', component: DetailPokemonComponent },
+  { path: '', redirectTo: 'pokemons', pathMatch: 'full' }
 ];
 
 @NgModule({
