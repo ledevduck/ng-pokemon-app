71c150a76832d9b185c09dc73037de7fce8827a5
“ledevduck” <devduck@tutanota.com>
2022-07-15 10:46:11 +0200

    use @Input to update border color

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[pkmnBorderCard]'
@@ -9,8 +9,10 @@ export class BorderCardDirective {
     this.setHeight(180);
   }
 
+  @Input('pkmnBorderCard') borderColor: string;
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder('#009688');
+    this.setBorder(this.borderColor || '#009688');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
