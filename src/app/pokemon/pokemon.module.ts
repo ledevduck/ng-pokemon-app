cef60cfe8be5166b46d32e6c8b7a2460953fa918
“ledevduck” <devduck@tutanota.com>
2022-07-15 18:23:41 +0200

    *FORMS* import FormsModule in app.module.ts for authentication form and import again in pokemonModule because we will use a form in component attached to pokemonModule

--- src/app/pokemon/pokemon.module.ts
+++ src/app/pokemon/pokemon.module.ts
@@ -6,6 +6,7 @@ import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
 import { RouterModule, Routes } from '@angular/router';
 import { PokemonService } from './pokemon.service';
+import { FormsModule } from '@angular/forms';
 
 const pokemonRoutes: Routes = [
   { path: 'pokemons', component: ListPokemonComponent },
@@ -21,6 +22,7 @@ const pokemonRoutes: Routes = [
   ],
   imports: [
     CommonModule,
+    FormsModule,
     RouterModule.forChild(pokemonRoutes)
   ],
   providers: [PokemonService]
