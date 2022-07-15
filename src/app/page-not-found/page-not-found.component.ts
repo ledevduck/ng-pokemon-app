03e5c114ff5f1eb73d9b2bcec98e0302c104e0ca
“ledevduck” <devduck@tutanota.com>
2022-07-15 13:17:04 +0200

    set page-not-found template

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-not-found',
   template: `
-    <p>
-      page-not-found works!
-    </p>
-  `,
-  styles: [
-  ]
+  <div class='center'>
+    <img src="http://assets.pokemon.com/assets/cms2/img/pokedex/full/035.png"/>
+    <h1>Hey, cette page n'existe pas !</h1>
+    <a routerLink="/pokemons" class="waves-effect waves-teal btn-flat">
+      Retourner à l' accueil
+    </a>
+  </div>
+`
 })
 export class PageNotFoundComponent implements OnInit {
 
