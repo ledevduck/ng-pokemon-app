dc49bcfbd89d752b06dc5c40d4876d71e3521bb0
“ledevduck” <devduck@tutanota.com>
2022-07-17 16:29:21 +0200

    set guard to use isLoggedIn boolean to redirect to the correct page

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(){
-    console.log('Le guard a bien été appelé !');
-    return true;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  canActivate(): boolean {
+    if (this.authService.isLoggedIn) {
+      return true;
+    }
+
+    this.router.navigate(['/login']);
+    return false;
   }
 }
