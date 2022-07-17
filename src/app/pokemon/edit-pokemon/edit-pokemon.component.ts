cfcb129f7c58dabccc8fc33867b0f1ddf731fc8c
“ledevduck” <devduck@tutanota.com>
2022-07-17 09:24:52 +0200

    set edit-pokemon class and template, separate from form component for more flexibility because we want to create new component to add new pokemons that will be using the form component

--- src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
+++ src/app/pokemon/edit-pokemon/edit-pokemon.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-edit-pokemon',
   template: `
-    <p>
-      edit-pokemon works!
+    <h2>Editer {{ pokemon?.name }}</h2>
+    <p *ngIf="pokemon" class="center">
+      <img [src]="pokemon.picture" />
     </p>
+    <app-pokemon-form *ngIf="pokemon" [pokemon]="pokemon"></app-pokemon-form>
   `,
   styles: [
   ]
 })
 export class EditPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemon: Pokemon | undefined;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) { }
+
+  ngOnInit() {
+    const pokemonId: string | null = this.route.snapshot.paramMap.get('id');
+    if (pokemonId) {
+      this.pokemon = this.pokemonService.getPokemonById(+pokemonId);
+    } else {
+      this.pokemon = undefined;
+    }
   }
 
 }
