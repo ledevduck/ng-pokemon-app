5575c53741dc050e24f0c7c5b74ce06312dd2a09
“ledevduck” <devduck@tutanota.com>
2022-07-17 08:54:44 +0200

    set pokemon-form class and template

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-pokemon-form',
-  templateUrl: './pokemon-form.component.html',
-  styles: [
-  ]
+  templateUrl: './pokemon-form.component.html'
 })
 export class PokemonFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() pokemon: Pokemon;
 
-  ngOnInit(): void {
+  types: string[];
+
+  constructor(
+    private pokemonService: PokemonService,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    this.types = this.pokemonService.getPokemonTypeList();
+  }
+
+  hasType(type: string): boolean {
+    return this.pokemon.types.includes(type);
+  }
+
+  selectType($event: Event, type: string) {
+    const isChecked = ($event.target as HTMLInputElement).checked;
+
+    if (isChecked) {
+      this.pokemon.types.push(type);
+    } else {
+      const index = this.pokemon.types.indexOf(type);
+      this.pokemon.types.splice(index, 1);
+    }
+  }
+
+  isTypesValid(type: string): boolean {
+    if (this.pokemon.types.length == 1 && this.hasType(type)) {
+      return false;
+    }
+
+    if (this.pokemon.types.length > 2 && !this.hasType(type)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  onSubmit() {
+    console.log('Submit form !');
+    this.router.navigate(['/pokemon', this.pokemon.id]);
   }
 
 }
