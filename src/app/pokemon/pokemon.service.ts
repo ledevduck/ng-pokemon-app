a91d45b51d9204cfafa2eba88d7638bdd5033005
“ledevduck” <devduck@tutanota.com>
2022-07-17 10:52:28 +0200

    modify getPokemonList() method from pokemon.service to simulate request to a distant api using async (observable), more infos at 6h25m

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,12 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { POKEMONS } from './mock-pokemon';
 import { Pokemon } from './pokemon';
 
 @Injectable()
 export class PokemonService {
 
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  constructor(private http: HttpClient) {}
+
+  getPokemonList(): Observable<Pokemon[]> {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap((response) => console.table(response)),
+      catchError((error) => {
+        console.log(error);
+        return of([]);
+      })
+    )
   }
 
   getPokemonById(pokemonId: number): Pokemon | undefined {
