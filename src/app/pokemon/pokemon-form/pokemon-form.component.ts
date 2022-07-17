feef3165df95d54a0faf2879d0298e4d61b669f2
“ledevduck” <devduck@tutanota.com>
2022-07-17 13:44:51 +0200

    update onSubmit method: if isAddForm true call addPokemon method, else call updatePokemon method, subscribe to redirect to the right page

--- src/app/pokemon/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon/pokemon-form/pokemon-form.component.ts
@@ -53,8 +53,13 @@ export class PokemonFormComponent implements OnInit {
   }
 
   onSubmit() {
-    this.pokemonService.updatePokemon(this.pokemon)
+    if (this.isAddForm) {
+      this.pokemonService.addPokemon(this.pokemon)
+        .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    } else {
+      this.pokemonService.updatePokemon(this.pokemon)
       .subscribe(() => this.router.navigate(['/pokemon', this.pokemon.id]));
+    }
   }
 
 }
