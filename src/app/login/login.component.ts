ee35a0dfc9464996aa2a6b32d5ca86e8392da5bf
“ledevduck” <devduck@tutanota.com>
2022-07-17 16:58:57 +0200

    set login component, 8h30-45m

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  message: string = 'Vous êtes déconnecté. (pikachu/pikachu)';
+  name: string;
+  password: string;
+  auth: AuthService;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
+
+  ngOnInit() {
+    this.auth = this.authService;
+  }
+
+  setMessage() {
+    if (this.auth.isLoggedIn) {
+      this.message = 'Vous êtes connecté.';
+    } else {
+      this.message = 'Identifiant ou mot de passe incorrect.'
+    }
+  }
+
+  login() {
+    this.message = "Tentative de connection en cours...";
+    this.auth.login(this.name, this.password)
+      .subscribe((isLoggedIn: boolean) => {
+        this.setMessage();
+        if (isLoggedIn) {
+          this.router.navigate(['/pokemons']);
+        } else {
+          this.password = '';
+          this.router.navigate(['/login']);
+        }
+      });
+  }
+
+  logout() {
+    this.auth.logout();
+    this.message = 'Vous êtes déconnecté.';
   }
 
 }
