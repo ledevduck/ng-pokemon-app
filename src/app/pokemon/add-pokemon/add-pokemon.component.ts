6ff278b9408f1b23e3d36201480dc6598257322a
“ledevduck” <devduck@tutanota.com>
2022-07-17 13:03:55 +0200

    set add pokemon template to display a title and the app pokemon form component

--- src/app/pokemon/add-pokemon/add-pokemon.component.ts
+++ src/app/pokemon/add-pokemon/add-pokemon.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-add-pokemon',
   template: `
-    <p>
-      add-pokemon works!
-    </p>
+    <h2 class="center">Ajouter un pokémon</h2>
+    <app-pokemon-form [pokemon]="pokemon"></app-pokemon-form>
   `,
   styles: [
   ]
 })
 export class AddPokemonComponent implements OnInit {
 
-  constructor() { }
+  pokemon: Pokemon;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pokemon = new Pokemon();
   }
 
 }
