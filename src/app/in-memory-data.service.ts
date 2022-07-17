e9332c0796f5434648a648759f3d0ff7ec646b3f
“ledevduck” <devduck@tutanota.com>
2022-07-17 11:40:26 +0200

    delete useless import

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
 import { POKEMONS } from './pokemon/mock-pokemon';
 
 @Injectable({
