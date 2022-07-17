775d6816798c963457bb89465e868badd4d4c33a
“ledevduck” <devduck@tutanota.com>
2022-07-17 11:21:40 +0200

    update detail pokemon onInit for similar reasons as the precedent commit

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -18,7 +18,7 @@ export class ListPokemonComponent implements OnInit {
 
   ngOnInit() {
     this.pokemonService.getPokemonList()
-      .subscribe(pokemonList => this.pokemonList = pokemonList)
+      .subscribe(pokemonList => this.pokemonList = pokemonList);
   }
 
   goToPokemon(pokemon: Pokemon) {
