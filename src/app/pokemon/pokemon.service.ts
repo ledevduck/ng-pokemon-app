a6858ae74130bbabe0b9695461ba6f43d4fbe782
“ledevduck” <devduck@tutanota.com>
2022-07-17 11:36:48 +0200

    typo correction, detail page was not working

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -16,7 +16,7 @@ export class PokemonService {
   }
 
   getPokemonById(pokemonId: number): Observable<Pokemon | undefined> {
-    return this.http.get<Pokemon>(`api/pokemon/${pokemonId}`).pipe(
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
       tap((response) => this.log(response)),
       catchError((error) => this.handleError(error, undefined))
     );
