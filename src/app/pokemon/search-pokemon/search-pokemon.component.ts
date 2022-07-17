8a3c7ed03a6355f333b8c558a1099c37400a6a07
“ledevduck” <devduck@tutanota.com>
2022-07-17 15:07:34 +0200

    set search pokemon class ngOnInit to display search results as user is typing, 7h55m

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from 'rxjs';
 import { Pokemon } from '../pokemon';
+import { PokemonService } from '../pokemon.service';
 
 @Component({
   selector: 'app-search-pokemon',
@@ -12,9 +13,17 @@ export class SearchPokemonComponent implements OnInit {
   searchTerms = new Subject<string>();
   pokemons$: Observable<Pokemon[]>;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private pokemonService: PokemonService
+    ) { }
 
   ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term) => this.pokemonService.searchPokemonList(term))
+    )
   }
 
   search(term: string) {
