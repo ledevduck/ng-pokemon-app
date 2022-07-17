00b977c63c8fa1d9253d5c53918843a05b1adfa4
“ledevduck” <devduck@tutanota.com>
2022-07-17 12:32:06 +0200

    add deletePokemon method with redirection to pokemons list page

--- src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
+++ src/app/pokemon/detail-pokemon/detail-pokemon.component.ts
@@ -27,6 +27,11 @@ export class DetailPokemonComponent implements OnInit {
     }
   }
 
+  deletePokemon(pokemon: Pokemon) {
+    this.pokemonService.deletePokemonById(pokemon.id)
+      .subscribe(() => this.goToPokemonList());
+  }
+
   goToPokemonList() {
     this.router.navigate(['/pokemons']);
   }
